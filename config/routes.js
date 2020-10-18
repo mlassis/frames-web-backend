c2e56ac44abae074ca85461dfd6c494704db49ad
Mario Assis <mlassis2009@gmail.com>
2020-10-18 19:33:14 -0300

    Ajuste aquivo routes

--- config/routes.js
+++ config/routes.js
@@ -6,11 +6,6 @@ module.exports = function (server) {
     const router = express.Router()
     server.use('/api', router)
 
-    //router.route('/teste').get(function (req, res, next) {
-      //res.send('funcinou a porra do get')
-    //})
-
-
     //Rotas da API
     const billingCycleService = require('../api/billingCycle/billingCycleService');
     billingCycleService.register(router, '/billingCycles')
