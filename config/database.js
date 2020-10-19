faa36ec49864f53ca0857dcaea6589a8574c4c60
Mario Assis <mlassis2009@gmail.com>
2020-10-18 23:51:30 -0300

    Acrescentando arquivo no termino do backend

--- config/database.js
+++ config/database.js
@@ -1,2 +1,6 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', {useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.Error.messages.general.require = "O atributo '{PATH}'é obrigatório"
+mongoose.Error.messages.Number.mim = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'"
--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,12 @@ module.exports = function (server) {
 
     //Api router
     const router = express.Router()
-    server.use('/api', router)
+    server.use('/api',router)
 
     // rotas da API
     const billingCycleService = require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
