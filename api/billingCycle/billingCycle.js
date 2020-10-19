fd69088d1cd06818506c3559e29ffb4be852c301
Mario Assis <mlassis2009@gmail.com>
2020-10-18 21:16:41 -0300

    Ajustando os erros

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -17,7 +17,7 @@ const debtSchema = new mongoose.Schema({
 
 const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 2, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
     debts: [debtSchema]
--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 module.exports = function (server) {
 
-    //API Routes
+    //Api router
     const router = express.Router()
     server.use('/api', router)
 
-    //Rotas da API
-    const billingCycleService = require('../api/billingCycle/billingCycleService');
+    // rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router, '/billingCycles')
 }
