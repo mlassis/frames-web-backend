200a6ee4994f263edbcfc609734fe6a8dfcc3547
Mario Assis <mlassis2009@gmail.com>
2020-10-19 15:51:32 -0300

    Erros ajustados 100% funcionando o backend

--- api/billingSummary/billingSummaryService.js
+++ api/billingSummary/billingSummaryService.js
@@ -1,15 +1,15 @@
 const _ = require('lodash')
-const  BillingCycle = require('../billingCycles/billingCycle')
+const BillingCycle = require('../billingCycle/billingCycle')
 
 //Mais uma função Middleware
 function getSummary(req, res){
-  BillingCycle.aggregate({
+  BillingCycle.aggregate([{
     $project: { credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}},
   },{
       $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
     },{
       $project: {_id: 0, credit: 1, debt: 1}
-    }, function(error, result){
+    }], function(error, result){
       if(error){
         res.status(500).json({errors: [error]})
       }else{
